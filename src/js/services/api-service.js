6f5a10d0bd354fa3bda9f92f82ad87380aabccc9
Vladyslav Turchenko <86655268+agatohn@users.noreply.github.com>
2021-09-07 17:55:00 +0300

    Update api-service.js

--- src/js/services/api-service.js
+++ src/js/services/api-service.js
@@ -8,12 +8,7 @@ const fetchCountry = country => {
     .then(res => res.json())
     .then(data => {
       console.log(data);
-      if (data.length === 1) {
-        return data[0];
-      }
-      // if (data.length >= 2 || data.length <= 10) {
-      //   return data;
-      // }
+      return data;
     });
 };
 
